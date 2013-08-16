fa2ee9a71aeb230d0d58cbf8b65c40f7aeaff5d0
Rian Hunter <rian@alum.mit.edu>
2013-08-15 17:58:24 -0700

    windows makefile

--- config-nt-msvcrt-mingw.mk
+++ config-nt-msvcrt-mingw.mk
@@ -1,14 +1,8 @@
 # Customize below to fit your system
 # This example is made for nt/msvcrt/mingw
 FDEVENT_IMPL = select
-FSTATAT_IMPL = none
 SOCKETS_IMPL = winsock
 FS_IMPL = win32
-WEBDAV_BACKEND_IMPL = fs
-HTTP_BACKEND_IMPL = sockets_fdevent
-HTTP_BACKEND_SOURCES = http_backend_sockets_fdevent.c fdevent_select.c sockets_winsock.c util_sockets.c
-WEBDAV_BACKEND_SOURCES = webdav_backend_fs.c fs_win32.c dfs.c util_fs.c
-
 SOCKETS_LDFLAGS = -lws2_32
 
 # flags
